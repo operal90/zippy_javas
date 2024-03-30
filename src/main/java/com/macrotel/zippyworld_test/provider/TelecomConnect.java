1c33ba8f792c731522c1e4c525e33a880a489d60
nhurudheen <ifaniyioluwapelumi@gmail.com>
2024-03-31 00:55:48 +0100

    Airtime Recharge Successful

--- src/main/java/com/macrotel/zippyworld_test/provider/TelecomConnect.java
+++ src/main/java/com/macrotel/zippyworld_test/provider/TelecomConnect.java
@@ -32,44 +32,34 @@ public class TelecomConnect {
 
     private Object airtimeVending(String phoneNumber, int amount, String transactionId, String networkUrl){
         HashMap<String, Object> result = new HashMap<>();
-//        String airtimeUrl =  END_POINT_TLS+networkUrl;
-//        Map<String, String> headers = new HashMap<>();
-//        headers.put("x-api-key", X_API_KEY_TLS);
-//        headers.put("client-id", CLIENT_ID_TLS);
-//        headers.put("Content-Type", " application/x-www-form-urlencoded");
-//
-//
-//
-//        MultiValueMap<String, String> formParams = new LinkedMultiValueMap<>();
-//        formParams.add("phonenumber", phoneNumber);
-//        formParams.add("amount", String.valueOf(amount));
-//        formParams.add("transaction_id", transactionId);
-//
-//        Object airtimeVendingThirdParty = thirdPartyAPI.callAPI(airtimeUrl, HttpMethod.POST,headers,formParams);
-//        if(airtimeVendingThirdParty == null){
-//            result.put("statusCode", "1");
-//            result.put("details", new ArrayList<>());
-//        }
-//        else{
-//            Map<String, Object> apiResponse = (Map<String, Object>) airtimeVendingThirdParty;
-//            String statusCode = (String) apiResponse.get("status_code");
-//            if(Objects.equals(statusCode, "0")){
-//                result.put("statusCode", "0");
-//                result.put("details", apiResponse);
-//            }
-//            else{
-//                result.put("statusCode", "1");
-//                result.put("details", apiResponse);
-//            }
-//        }
-        HashMap<String, String> apiResponse = new HashMap<>();
-        apiResponse.put("status_code","0");
-        apiResponse.put("message", "Successful");
-        apiResponse.put("reference_number", "2024033023209799");
-        apiResponse.put("confirm_status","Successful");
-        apiResponse.put("network", "Glo");
-        result.put("statusCode", "0");
-        result.put("details", apiResponse);
+        String airtimeUrl =  END_POINT_TLS+networkUrl;
+        Map<String, String> headers = new HashMap<>();
+        headers.put("x-api-key", X_API_KEY_TLS);
+        headers.put("client-id", CLIENT_ID_TLS);
+        headers.put("Content-Type", " application/x-www-form-urlencoded");
+
+        MultiValueMap<String, String> formParams = new LinkedMultiValueMap<>();
+        formParams.add("phonenumber", phoneNumber);
+        formParams.add("amount", String.valueOf(amount));
+        formParams.add("transaction_id", transactionId);
+
+        Object airtimeVendingThirdParty = thirdPartyAPI.callAPI(airtimeUrl, HttpMethod.POST,headers,formParams);
+        if(airtimeVendingThirdParty == null){
+            result.put("statusCode", "1");
+            result.put("details", new ArrayList<>());
+        }
+        else{
+            Map<String, Object> apiResponse = (Map<String, Object>) airtimeVendingThirdParty;
+            String statusCode = (String) apiResponse.get("status_code");
+            if(Objects.equals(statusCode, "0")){
+                result.put("statusCode", "0");
+                result.put("details", apiResponse);
+            }
+            else{
+                result.put("statusCode", "1");
+                result.put("details", apiResponse);
+            }
+        }
         return result;
     }
 }
